febd133cd151a107bd3e3a6879c451f8f7c2d9ff
brofeelgood <118669907+brofeelgood@users.noreply.github.com>
2025-01-12 20:43:50 +0800

    Update light_out_control_select.cpp

--- components/LD2412/select/light_out_control_select.cpp
+++ components/LD2412/select/light_out_control_select.cpp
@@ -5,7 +5,7 @@ namespace LD2412 {
 
 void LightOutControlSelect::control(const std::string &value) {
   this->publish_state(value);
-  this->parent_->set_light_out_control(state);
+  this->parent_->set_light_out_control();
 }
 
 }  // namespace LD2412
